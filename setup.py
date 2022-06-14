8f0ffc9368d68a8041ccebd690e656bcf91d6499
Keeley Erhardt <keeley@mit.edu>
2022-06-14 12:59:43 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='influence',
+    version='0.1.0',
+    packages=['influence'],
+    url='https://github.com/keelerh/influence',
+    license='MIT License',
+    author='Keeley Erhardt',
+    author_email='keeley@mit.edu',
+    description='Designed to support the development of influence models, a type of networked, discrete-time stochastic models'
+)
